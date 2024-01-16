d7a736f2d6e5e3a60a032a0a25d52ff70686b6ee
Bob Hodges <bobhodges1@outlook.com>
2024-01-16 13:39:57 +0000

    fixed code

--- src/find-shortest.js
+++ src/find-shortest.js
@@ -7,18 +7,14 @@
  * @param {string[]} stringArr - array of strings
  * @returns {string} - the shortest string
  */
-function findShortestString(
-stringArr
-)
-{ const shortestString = stringArr[0]
-    for ((((let str of stringArr)))) {
-if (str.length < shortestString.length) { shortestString = str
-    }}  return shortestString.length    
-
-
-
-
-
+function findShortestString(stringArr) {
+  const shortestString = stringArr[0];
+  for (let str of stringArr) {
+    if (str.length < shortestString.length) {
+      shortestString = str;
+    }
+  }
+  return shortestString.length;
 }
 
 console.log(findShortestString(["it", "is", "a", "nice", "day"]), "a");
